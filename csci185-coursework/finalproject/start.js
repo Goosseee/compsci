f102a96871755c1fd54483d8acac894e8f6939ae
dillan <dill.yoxall@gmail.com>
2023-01-18 14:27:56 -0500

    csci344 tutorial 2

---
+++ csci185-coursework/finalproject/start.js
@@ -0,0 +1,19 @@
+// function setup(){
+//     const canvasWidth = window.innerWidth;
+//     const canvasHeight = window.innerHeight; 
+//     createCanvas(canvasWidth, canvasHeight);
+//     drawGrid(canvasWidth, canvasHeight);
+// }
+
+// // const img = new Image(300, 400); // width, height
+// // img.src = "images/shootingstar.png";
+// // document.body.appendChild(img);
+
+
+// // var buttons = document.getElementById("button");
+// // buttons.innerHTML = '<img src="images/shootingstar.png" width="500" height="250"/>';
+
+
+function transition(){
+    document.querySelector('')
+}
